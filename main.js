c0412e4a0efde8afd5c072f515f46de9f5e62ec8
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-05-07 01:15:00 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      reg.showNotification('Hello world!');
+    });
+  }
+} 
